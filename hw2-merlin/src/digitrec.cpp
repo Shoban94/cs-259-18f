72e82e03e07350c44c6df132180dbde2a4d57bde
Cloud User <centos@ip-172-31-38-147.ec2.internal>
2018-10-27 23:44:37 -0700

    lab2-part2: first version

--- hw2-merlin/src/digitrec.cpp
+++ hw2-merlin/src/digitrec.cpp
@@ -2,29 +2,39 @@
 #include <math.h>
 #include <assert.h>
 
+#pragma ACCEL kernel
 void digitrec_kernel(
         unsigned long test_image,
         unsigned long* train_images,
         unsigned char* knn_mat) {
+#pragma ACCEL interface variable=train_images depth=18000 bus_bitwidth=512
+#pragma ACCEL interface variable=knn_mat depth=30 bus_bitwidth=512
+#pragma ACCEL interface variable=buf_knn_mat depth=30 bus_bitwidth=512
 
     unsigned char buf_knn_mat[10][3];
 
+
+#pragma ACCEL parallel 
     for (int x = 0; x < 10; ++x) {
         for (int y = 0; y < 3; ++y) {
             buf_knn_mat[x][y] = 50;
         }
     }
-
+#pragma ACCEL parallel
     for (int x = 0; x < 10; ++x) {
-        for (int y = 0; y < 1800; ++y) {
+#pragma ACCEL tiling factor=360
+ Loopsss:  for (int y = 0; y < 1800; ++y) {
             unsigned long temp = train_images[x * 1800 + y] ^ test_image;
             unsigned char dis = 0;
+#pragma ACCEL parallel 
+
             for (int i = 0; i < 49; ++i) {
                 dis += (temp & (1L << i)) >> i;
             }
 
             unsigned char max_id = 0;
             unsigned char max_val = 0;
+
             for (int i1 = 0; i1 < 3; ++i1) {
                 if (max_val < buf_knn_mat[x][i1]) {
                     max_id = i1;
@@ -36,7 +46,7 @@ void digitrec_kernel(
             }
         }
     }
-
+#pragma ACCEL parallel
     for (int x = 0; x < 10; ++x) {
         for (int y = 0; y < 3; ++y) {
             knn_mat[x * 3 + y] = buf_knn_mat[x][y];
